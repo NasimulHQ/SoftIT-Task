41394893c7f54e6a075c007b6fbf1d8a5391698a
NasimulHQ <nasimul345@gmail.com>
2022-06-06 01:14:10 +0600

    first commit & table migration & employee seeder, factory

---
+++ resources/views/frontEnd/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Job Task</title>
+</head>
+<body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6" style="text-align: center; margin-top: 100px">
+                    <h1>HELLO, Soft IT Care!</h1>
+                    <h5>This is Nasimul Haque Pathan.</h5>
+                    <a class="btn btn-primary" href="">Problem 01</a>
+                    <a class="btn btn-success" href="">Problem 02</a>
+                    <a class="btn btn-primary" href="">Problem 03</a>
+                    <a class="btn btn-success" href="">Problem 04</a>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+ 
+</body>
+</html>
